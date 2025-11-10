4027eb34e6c5ef5b5010a3b357db3a7e28da19a5
Kwigorl <143103853+Kwigorl@users.noreply.github.com>
2025-11-10 22:29:14 +0100

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,22 @@
+import discord
+from discord.ext import commands
+import asyncio
+import os
+
+# Import du module d'emprunts
+import emprunts
+
+TOKEN = os.getenv("DISCORD_TOKEN")  # ton token Discord sur Render
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+async def main():
+    async with bot:
+        # Charge le cog d'emprunts
+        await bot.load_extension("emprunts")
+        await bot.start(TOKEN)
+
+asyncio.run(main())
