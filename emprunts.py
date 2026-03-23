1b73f76af9a7a6281cb3287dd5e6428ccb2ced94
Kwigorl <143103853+Kwigorl@users.noreply.github.com>
2026-03-23 11:26:02 +0100

    Update emprunts.py

--- emprunts.py
+++ emprunts.py
@@ -25,7 +25,7 @@ CRENEAUX = [
     {"jour": 2, "start": 20, "end": 24},  # Mercredi 20h-24h
     {"jour": 4, "start": 20, "end": 24},  # Vendredi 20h-24h
     {"jour": 6, "start": 14, "end": 18},  # Dimanche 14h-18h
-    {"jour": 7, "start": 10, "end": 20}   # Test Lundi 10h-20h
+    {"jour": 0, "start": 10, "end": 20}   # Test Lundi 10h-20h
 ]
 
 # --------------------------
